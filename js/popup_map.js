345b2aa0a3f6f604d32e5505b7811b32d2152e0c
Arseniy Khmelevoy <kekcij@mail.ru>
2023-05-14 12:33:14 +0300

    Removed extra files + Bug fix

--- js/popup_map.js
+++ js/popup_map.js
@@ -5,18 +5,25 @@ const mapClose = mapModal.querySelector(".modal-close")
 
 mapLink.addEventListener("click", function (evt) {
     evt.preventDefault()
+    mapModal.classList.remove("modal-hide")
     mapModal.classList.add("modal-show")
 })
 
 mapClose.addEventListener("click", function (evt) {
     evt.preventDefault()
-    mapModal.classList.remove("modal-show")
+    mapModal.classList.add("modal-hide")
+    setTimeout(() => {
+        mapModal.classList.remove("modal-show")
+    }, 550);
 })
 
 window.addEventListener("keydown", function (evt) {
     if (evt.key === "Escape") {
         if (mapModal.classList.contains("modal-show")) {
-            mapModal.classList.remove("modal-show")
+            mapModal.classList.add("modal-hide")
+            setTimeout(() => {
+                mapModal.classList.remove("modal-show")
+            }, 550);
         }
     }
 })
